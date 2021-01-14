6539da08925f005359ebf4e1a8472af326b2c120
Jônatas Souza Soares <56240097+sozinhoL@users.noreply.github.com>
2021-01-14 20:05:43 -0300

    Update index.js

--- index.js
+++ index.js
@@ -23,9 +23,11 @@ io.on('connection', (socket) => {
   // when the client emits 'new message', this listens and executes
   socket.on('new message', (data) => {
     // we tell the client to execute 'new message'
+    console.log(data);
+    var message = data.replace("puta", "linda");
     socket.broadcast.emit('new message', {
       username: socket.username,
-      message: data
+      message: message
     });
   });
 
